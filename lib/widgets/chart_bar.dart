97284d5463c00d3f4d79f7ca87ac5aa3783be32a
João Pedro Nardari <joaonardari@gmail.com>
2020-05-03 16:37:51 -0300

    Chart Position

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -15,8 +15,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          ),
         ),
         SizedBox(
           height: 4,
@@ -37,6 +40,7 @@ class ChartBar extends StatelessWidget {
                 heightFactor: percentOfTotal,
                 child: Container(
                   decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
                     color: Theme.of(context).primaryColor,
                     borderRadius: BorderRadius.circular(10),
                   ),
